5fa43b2eabe80ddaa80d519fe42f134e802bb1c8
Peter Chege <peterkagure@gmail.com>
2023-01-21 17:35:26 +0300

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.30"
+    id("org.jetbrains.compose") version "0.3.1"
+}
+
+group = "me.peter"
+version = "1.0"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    google()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    implementation ("com.google.android.exoplayer:exoplayer:2.11.7")
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "composeDesktopApp"
+            packageVersion = "1.0.0"
+        }
+    }
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,24 @@
+import androidx.compose.desktop.Window
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+fun main() = Window {
+    var text by remember { mutableStateOf("Hello, World!") }
+    var textfield by remember { mutableStateOf("") }
+
+    MaterialTheme {
+
+
+    }
+}
