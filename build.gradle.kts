e7572dd563963673244942dad11139b28f19d75a
chege4179 <peterkagure@gmail.com>
2023-11-12 01:27:12 +0300

    Some minor changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.30"
-    id("org.jetbrains.compose") version "0.3.1"
+    kotlin("jvm") version "1.9.0"
+    id("org.jetbrains.compose") version "1.6.0-dev1265"
 }
 
 group = "me.peter"
@@ -20,10 +20,11 @@ repositories {
 dependencies {
     implementation(compose.desktop.currentOs)
     implementation ("com.google.android.exoplayer:exoplayer:2.11.7")
+    implementation("uk.co.caprica:vlcj:4.7.0")
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "17"
 }
 
 compose.desktop {
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,11 @@
-import androidx.compose.desktop.Window
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,25 +14,38 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 
-fun main() = Window {
-    var text by remember { mutableStateOf("Hello, World!") }
-    var textfield by remember { mutableStateOf("") }
+fun main(){
+    return application {
+        Window(
+            onCloseRequest = { exitApplication() },
+            title = "Compose Desktop App",
+            state = rememberWindowState(
+                position = WindowPosition.Aligned(Alignment.Center),
+                width = 1200.dp,
+                height = 700.dp,
+            ),
+        ){
+            MaterialTheme {
+                val name = remember { mutableStateOf("") }
+                Column  (
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    VideoPlayerImpl(
+                        url = "https://res.cloudinary.com/dhuqr5iyw/video/upload/v1636615405/movies/FnVideo.mp4",
+                        modifier = Modifier.fillMaxWidth()
 
-    MaterialTheme {
-        Column  (
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+                    )
+                }
 
-            Button(onClick = {
-
-            }){
-                Text(text = "Hello World")
             }
         }
-
     }
 }
